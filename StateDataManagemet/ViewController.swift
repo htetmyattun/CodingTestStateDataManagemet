c3de9ba53e09a47907c19f52fc0cdcd99a66ef66
Htet Myat <pokemonfake11111@gmail.com>
2023-05-30 09:13:44 +0630

    Create Project

---
+++ StateDataManagemet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StateDataManagemet
+//
+//  Created by Htet Myat Tun on 30/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
